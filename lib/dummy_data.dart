76c74234bebc5df86f387b55883283daaf634817
Tarikul-islam-shazan <Tarikul@brainstation23.com>
2021-07-24 02:43:36 +0600

    project initalization : categories and category screen

---
+++ lib/dummy_data.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'French',
+    color: Colors.pink,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Summer',
+    color: Colors.teal,
+  ),
+  Category(
+    id: 'c4',
+    title: 'American',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Mexican',
+    color: Colors.lightBlue,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Arabic',
+    color: Colors.limeAccent,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Mexican',
+    color: Colors.lightBlue,
+  ),
+  Category(
+    id: 'c8',
+    title: 'Arabic',
+    color: Colors.limeAccent,
+  ),
+  Category(
+    id: 'c9',
+    title: 'Mexican',
+    color: Colors.lightBlue,
+  ),
+  Category(
+    id: 'c10',
+    title: 'Arabic',
+    color: Colors.limeAccent,
+  ),
+  Category(
+    id: 'c11',
+    title: 'Arabic',
+    color: Colors.limeAccent,
+  ),
+  Category(
+    id: 'c12',
+    title: 'Mexican',
+    color: Colors.lightBlue,
+  ),
+  Category(
+    id: 'c13',
+    title: 'Arabic',
+    color: Colors.limeAccent,
+  ),
+];
